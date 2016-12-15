c4ed51a847e1fecdcb21edebbe7e49f7e94a5d57
Eric Satterwhite <esatterwhite@wi.rr.com>
2016-12-14 23:59:35 -0600

    funnel incomming http and proxies throught router

--- index.js
+++ index.js
@@ -4,13 +4,22 @@ const conf = require('keef')
     , http = require('http')
     , handler = require('./lib/handler')
     , ring = require('./lib/ring')
+		, Router = require('./lib/server')
+    , debug = require('debug')('kronos')
     ;
 
-const server = http.createServer( handler );
+const router = Router.load();
+const server = http.createServer((req, res) => {
+	router.handle( req, res );
+});
 
 if( require.main === module ){
 	server.listen(conf.get('PORT'));
 }
 
+ring.on('request', (req, res) => {
+  debug('proxied'); 
+  router.handle(req, res)
+});
 module.exports = { server, ring };
 
