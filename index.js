2d21f0ef056145ed1a884061ca75869eb6733aaa
Eric Satterwhite <esatterwhite@wi.rr.com>
2016-12-17 14:04:24 -0600

    make server and node to replace inline code

--- index.js
+++ index.js
@@ -3,25 +3,16 @@
 const conf = require('keef')
     , http = require('http')
     , Node = require('./lib/ring')
-		, Router = require('./lib/server')
+		, Server = require('./lib/server')
     , debug = require('debug')('kronos')
     ;
 
-const router = Router.load();
-const node = new Node();
-const server = http.createServer((req, res) => {
-	router.handle( req, res );
-});
-
-module.exports = { server, node };
+const server = new Server();
+module.exports = {server};
 
 if( require.main === module ){
-	server.listen(conf.get('PORT'));
-  node.join(null, (err)=>{
-    if(err) return console.log(err)
-    node.handle((req, res) => {
-      router.handle(req, res)
-    });
+	server.load().listen(conf.get('PORT'),null, null, (err) => {
+    if(err) return console.log(err) || process.exit(1)
   });
 }
 
