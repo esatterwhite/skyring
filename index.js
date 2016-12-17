8f5731aea24fada55c0d3b515df61ca74018e186
Eric Satterwhite <esatterwhite@wi.rr.com>
2016-12-17 10:53:39 -0600

    rework timer cache to store some meta data
    soe we can redistribute / replay them

--- index.js
+++ index.js
@@ -2,22 +2,26 @@
 
 const conf = require('keef')
     , http = require('http')
-    , ring = require('./lib/ring')
+    , Node = require('./lib/ring')
 		, Router = require('./lib/server')
     , debug = require('debug')('kronos')
     ;
 
 const router = Router.load();
+const node = new Node();
 const server = http.createServer((req, res) => {
 	router.handle( req, res );
 });
 
+module.exports = { server, node };
+
 if( require.main === module ){
 	server.listen(conf.get('PORT'));
+  node.join(null, (err)=>{
+    if(err) return console.log(err)
+    node.handle((req, res) => {
+      router.handle(req, res)
+    });
+  });
 }
-
-ring.on('request', (req, res) => {
-  router.handle(req, res)
-});
-module.exports = { server, ring };
 
