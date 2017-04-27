9a4f327250bd1e73db9c2312aaf074045acca830
Eric Satterwhite <esatterwhite@wi.rr.com>
2017-04-26 21:34:15 -0500

    conf: adding config docs

--- conf/index.js
+++ conf/index.js
@@ -1,6 +1,21 @@
 'use strict'
+/**
+ * Configuration options for skyring. See {@link module:keef} on ways to pass configuration
+ * @module skyring/conf
+ * @author Eric Satterwhite
+ * @since 1.0.0
+ **/
 
 module.exports = {
+/**
+ * @property {String|String[]} [seeds=127.0.0.1:3455,127.0.0.1:3456] A list of seed nodes to use for bootstrapping a ring cluster
+ * @property {Object} channel
+ * @property {String} [channel.host=127.0.0.1] hostname or ip addres for tchannel to listen on
+ * @property {Number} [channel.port=3455] Port number for tchannel to bind to
+ * @property {Number} [PORT=3000] The port number for the http API server to bind to
+ * @property {Object} nats Nats queue specific configuration
+ * @property {String|String[]} [nats.hosts=127.0.0.1:4222] host:port of instances of a nats cluster. One is usually enough.
+ **/
   seeds: ['127.0.0.1:3455', '127.0.0.1:3456']
 , channel: {
     host:'127.0.0.1'
