e7ad1591c964a3470465ea34cb9c8e000dfd7473
Eric Satterwhite <esatterwhite@wi.rr.com>
2017-06-03 15:06:47 -0500

    update docs for 4.3

--- docs/api/api_project.js
+++ docs/api/api_project.js
@@ -1,12 +1,12 @@
 define({
   "name": "skyring",
-  "version": "4.1.0",
+  "version": "4.2.1",
   "description": "Distributed timers as a service",
   "sampleUrl": false,
   "apidoc": "0.2.0",
   "generator": {
     "name": "apidoc",
-    "time": "2017-05-28T03:13:01.614Z",
+    "time": "2017-06-03T20:06:10.692Z",
     "url": "http://apidocjs.com",
     "version": "0.16.1"
   }
