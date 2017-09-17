887b86c7cb9b0534c642cddad096e4eac1097c5f
Eric Satterwhite <esatterwhite@wi.rr.com>
2017-09-16 20:19:13 -0500

    test: fix flaky unit test ( server )
    There was race between all of the request being handled, and the
    shutdown of the test server

--- test/unit/server.spec.js
+++ test/unit/server.spec.js
@@ -103,9 +103,9 @@ test('server', (t) => {
     })
 
     tt.test('should survive a lost node', ( ttt ) => {
-      ttt.plan(101)
+      ttt.plan(52)
       const request = supertest('http://localhost:5557');
-      const requests = Array.from(Array(50).keys())
+      const requests = Array.from(Array(25).keys())
       postback = http.createServer((req, res) => {
         const parsed = url.parse(req.url)
         const q = qs.parse(parsed.query)
@@ -133,7 +133,9 @@ test('server', (t) => {
           })
       }, (err) => {
         ttt.error(err)
-        sthree.close()
+        sthree.close(() => {
+          ttt.pass('server closed')
+        })
       })
     });
     tt.end()
