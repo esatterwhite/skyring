4f2724543ac5b1cf4fcba161ee9ee56320c368aa
Eric Satterwhite <esatterwhite@wi.rr.com>
2020-04-29 16:46:48 -0500

    scripts: fix change log script
    The ordering was incorrect. forces most recent commits first

--- scripts/changelog.sh
+++ scripts/changelog.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 printf "# Changelog\n\n"
 previous_tag=0
+for current_tag in $(git tag --sort=-creatordate)
+do
 
-for current_tag in $(git tag --sort=creatordate)
-  do
-    if [ "$previous_tag" != 0 ];then
-      tag_date=$(git log -1 --pretty=format:'%ad' --date=short ${previous_tag})
-      printf "## ${previous_tag} (${tag_date})\n\n"
-      git log ${current_tag}...${previous_tag} --pretty=format:'* %s [View](https://github.com/esatterwhite/skyring/commit/%H)' | grep -v Merge
-      printf "\n\n"
-    fi
-    previous_tag=${current_tag}
-  done
+if [ "$previous_tag" != 0 ];then
+    tag_date=$(git log -1 --pretty=format:'%ad' --date=short ${previous_tag})
+    printf "## ${previous_tag} (${tag_date})\n\n"
+    git log ${current_tag}...${previous_tag} --pretty=format:'* %s [View](https://github.com/esatterwhite/skyring/commit/%H)' | grep -v Merge
+    printf "\n\n"
+fi
+previous_tag=${current_tag}
+done
